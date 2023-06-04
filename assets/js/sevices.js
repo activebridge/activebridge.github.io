10bbd9846f3701430bd53641a36d5400a910813b
den <katatsu12@gmail.com>
2023-06-04 21:17:42 +0300

    new Favicon, flex responsive

---
+++ assets/js/sevices.js
@@ -0,0 +1,17 @@
+// let path = document.querySelector("path#main");
+// let pathLenght = document.getTotalLength();
+
+// path.style.strokeDasharray = pathLenght + ' ' + pathLenght;
+
+// path.style.strokeDashoffset = pathLenght;
+
+// window.addEventListener(
+//   "scroll",
+//   () => {
+//     document.body.style.setProperty(
+//       "--scroll",
+//       window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+//     );
+//   },
+//   false
+// );
