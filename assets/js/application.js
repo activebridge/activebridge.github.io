fffccd2db3df417a62882c5bac3f56cffa6d23e3
smoke-ck <2056sanya9snake@gmail.com>
2023-11-14 13:47:38 +0200

    Fix mobile navbar

--- assets/js/application.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    document.getElementById('header').classList.add('mobile-only');
-  } else {
-    document.getElementById('header').classList.add('desktop-only');
-  };
-});
