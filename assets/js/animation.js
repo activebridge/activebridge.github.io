9f730d5a6e21504d0fed79d00d242eafee0725a4
smoke-ck <2056sanya9snake@gmail.com>
2023-12-18 15:16:07 +0200

    Add View Transiton v2

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function initSnakeAnimation (svg_class_name, container_class_name) {
-    let container = document.querySelector(container_class_name)
-    let path = document.querySelector(svg_class_name);
-    let pathLength = path.getTotalLength();
+document.addEventListener("DOMContentLoaded", connectAnimationToPage);
+if (document.readyState !== "loading") { connectAnimationToPage(); }
 
-    path.style.strokeDasharray = pathLength + " " + pathLength;
-    path.style.strokeDashoffset = pathLength;
+function initSnakeAnimation (svg_class_name, container_class_name) {
+  let container = document.querySelector(container_class_name)
+  let path = document.querySelector(svg_class_name);
+  let pathLength = path.getTotalLength();
 
-    function scrollAnimation () {
-      let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop - container.offsetTop + 200) / document.documentElement.clientHeight;
-      let drawLength = pathLength * scrollPercentage;
-      path.style.strokeDashoffset = pathLength - drawLength;
-    }
+  path.style.strokeDasharray = pathLength + " " + pathLength;
+  path.style.strokeDashoffset = pathLength;
 
-    window.addEventListener('scroll', scrollAnimation);
+  function scrollAnimation () {
+    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop - container.offsetTop + 200) / document.documentElement.clientHeight;
+    let drawLength = pathLength * scrollPercentage;
+    path.style.strokeDashoffset = pathLength - drawLength;
   }
 
+  window.addEventListener('scroll', scrollAnimation);
+}
+
+function connectAnimationToPage () {
   if (document.querySelector('.solutions')) {
     initSnakeAnimation(".svg-path", ".solutions-first-animated-cards")
     initSnakeAnimation(".second-svg-path", ".solutions-second-animated-cards")
   } else {
     initSnakeAnimation(".services-animation-svg", ".services-animated_cards")
-    }
-});
+  }
+}
