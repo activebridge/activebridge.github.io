59dc3cf17bc207982cf8fbb3db8d8a7bb5334828
smoke-ck <2056sanya9snake@gmail.com>
2024-02-15 13:02:30 +0200

    Fix snake animation js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", connectAnimationToPage);
 if (document.readyState !== "loading") { connectAnimationToPage(); }
 
-function initSnakeAnimation (svg_class_name, container_class_name) {
-  let container = document.querySelector(container_class_name)
-  let path = document.querySelector(svg_class_name);
+function initSnakeAnimation (path, container) {
   let pathLength = path.getTotalLength();
 
   path.style.strokeDasharray = pathLength + " " + pathLength;
   path.style.strokeDashoffset = pathLength;
 
   function scrollAnimation () {
-    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop - container.offsetTop + 200) / document.documentElement.clientHeight;
+    let scrollHeigth = container.scrollHeight > 1800 ? container.scrollHeight - document.documentElement.clientHeight : document.documentElement.clientHeight
+    let scrollPercentage = (document.documentElement.scrollTop - container.offsetTop + 200) / scrollHeigth;
     let drawLength = pathLength * scrollPercentage;
     path.style.strokeDashoffset = pathLength - drawLength;
   }
@@ -19,10 +18,10 @@ function initSnakeAnimation (svg_class_name, container_class_name) {
 }
 
 function connectAnimationToPage () {
-  if (document.querySelector('.solutions')) {
-    initSnakeAnimation(".svg-path", ".solutions-first-animated-cards")
-    initSnakeAnimation(".second-svg-path", ".solutions-second-animated-cards")
-  } else {
-    initSnakeAnimation(".services-animation-svg", ".services-animated_cards")
-  }
+  animatioBlocks = document.querySelectorAll('#snake-animation-block')
+
+  animatioBlocks .forEach((element) => {
+    const svg = element.querySelector('.svg-path')
+    initSnakeAnimation(svg, element)
+  })
 }
