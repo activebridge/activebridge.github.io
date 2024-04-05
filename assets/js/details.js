3b70ce788474347bb807f92c9160b98385aaa5ff
smoke-ck <2056sanya9snake@gmail.com>
2024-04-05 10:15:54 +0300

    details.js connection: Wait until the entire page is fully loaded

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(() => {
+window.onload = () => {
+    setTimeout(() => {
     const details = document.querySelectorAll('details');
     if (details.length > 0) {
       details[0].setAttribute('open', true);
     }
   }, 300);
-});
+};
